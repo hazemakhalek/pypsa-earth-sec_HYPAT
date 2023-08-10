a7117c4676fbd09b291012e67b0e409d73a236bf
Eddy-JV <eddy.jalbout@gmail.com>
2023-08-10 17:50:08 +0200

    Included H2prosim shares + automation of runs

---
+++ modify_config_files_weekly.py
@@ -0,0 +1,25 @@
+
+import ruamel.yaml
+
+file_paths = ['/nfs/home/edd32710/projects/HyPAT/Brazil_cas/pypsa-earth-sec/config_2030_cons.yaml',
+            '/nfs/home/edd32710/projects/HyPAT/Brazil_cas/pypsa-earth-sec/config_2030_opt.yaml',
+            '/nfs/home/edd32710/projects/HyPAT/Brazil_cas/pypsa-earth-sec/config_2030_real.yaml',
+            '/nfs/home/edd32710/projects/HyPAT/Brazil_cas/pypsa-earth-sec/config_2050_cons.yaml',
+            '/nfs/home/edd32710/projects/HyPAT/Brazil_cas/pypsa-earth-sec/config_2050_opt.yaml',
+            '/nfs/home/edd32710/projects/HyPAT/Brazil_cas/pypsa-earth-sec/config_2050_real.yaml']
+
+
+# Read the YAML file
+yaml = ruamel.yaml.YAML()
+
+for file_path in file_paths:
+    with open(file_path, 'r') as file:
+        yaml_content = yaml.load(file)
+
+    # Modify the desired line (e.g., change a value)
+
+    yaml_content['export']['export_delivery'] = 'weekly'
+
+    # Write the updated YAML back to the file
+    with open(file_path, 'w') as file:
+        yaml.dump(yaml_content, file)
